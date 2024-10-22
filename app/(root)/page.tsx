185726e8c4b4800d8aef1ee1cdd322eb2685ef6e
sankrkall2 <gowrisankarkalla4@gmail.com>
2024-10-22 11:39:12 +0530

    prisma error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,6 +11,7 @@ import Navbar from "./_components/navbar";
 import { FloatingNav } from "@/components/ui/floating.navbar";
 import db from "@/lib/db";
 import { getStoreByUserId } from "@/data/store-service";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   // const isOpen = useModalStore((state) => state.isOpen);
@@ -22,11 +23,13 @@ export default async function Home() {
   //   }
   // }, [isOpen,  onOpen]);
   const user = await auth();
-  const store = await db.store.findFirst({
-    where: {
-      userId: user?.user?.id,
-    },
-  });
+  const store =
+    user?.user?.id &&
+    (await db.store.findFirst({
+      where: {
+        userId: user?.user?.id,
+      },
+    }));
 
   const storeId = store ? store.id : "new";
 
@@ -54,7 +57,7 @@ export default async function Home() {
       <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative ">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <Navbar storeId={storeId}/>
+        <Navbar storeId={storeId} />
         <div className="flex items-center justify-center  flex-col pt-32">
           <h1 className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
             WEB AGENCY
